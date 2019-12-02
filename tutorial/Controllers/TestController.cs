4a012d81abdbc2fbe6a6ca5c5915e14c1d91eb42
Pylyp Vynnykov <Pylyp_Vynnykov@epam.com>
2019-12-02 11:08:48 +0200

    add

--- tutorial/Controllers/TestController.cs
+++ tutorial/Controllers/TestController.cs
@@ -8,10 +8,6 @@ namespace tutorial.Controllers
 {
     public class TestController : Controller
     {
-        // GET: Test
-        public ActionResult Index()
-        {
-            return View();
-        }
+
     }
 }
