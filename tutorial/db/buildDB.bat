1c1c9de332040a14edd26a26180b090d2e74ccc0
Pylyp Vynnykov <Pylyp_Vynnykov@epam.com>
2019-12-02 10:26:21 +0200

    Add project files.

---
+++ tutorial/db/buildDB.bat
@@ -0,0 +1,7 @@
+@echo off
+SET DIR=%~d0%~p0%
+
+SET connstring="data source=.;initial catalog=Northwind;integrated security=True"
+SET repository.path="https://github.com/espiderinc/tutorial-demo-net.git"
+ 
+"%DIR%..\..\packages\roundhouse.0.8.6\bin\rh.exe" /c=%connstring% /vf=../bin/tutorial.dll /r=%repository.path% /env=LOCAL /ni /simple
---
+++ tutorial/db/up/002CreateTableCity.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE City (
+    "CityID"          INT        NOT NULL,
+    "CityName"        NCHAR (50) NOT NULL,
+    "CityDescription" NCHAR (50) NOT NULL,
+	"Country"		  NCHAR (50) Not NULL,
+    CONSTRAINT "PK_City" PRIMARY KEY NONCLUSTERED ("CityID" ASC)
+);
